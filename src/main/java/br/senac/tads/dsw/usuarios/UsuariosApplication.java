3a0cdc8001bba7bc827bc1405c3e602ce79b7d87
Everton Coutinho <everton.coutinho00@gmail.com>
2020-05-23 14:09:24 -0300

    Implementado consulta de papeis do banco e renderização na tela com Thymelea

--- src/main/java/br/senac/tads/dsw/usuarios/UsuariosApplication.java
+++ src/main/java/br/senac/tads/dsw/usuarios/UsuariosApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsuariosApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UsuariosApplication.class, args);
 	}
+	
 
 }
